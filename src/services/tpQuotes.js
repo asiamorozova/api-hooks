7c2ee98d62e18c8c558109172b450b77b52b70f2
Anastasia Morozova <morozova.asia@gmail.com>
2020-05-07 16:16:23 -0700

    fixed my services js

--- src/services/tpQuotes.js
+++ src/services/tpQuotes.js
@@ -1,6 +1,10 @@
 export const tpQuotes = () => {
   return fetch('https://thedavidbarton.herokuapp.com/api/1/quotes/recommend')
     .then(res => res.json())
-
-    .then(json => json.map(({ url }) => url));
+    .then(res => res.results.map(quotes =>({
+      id: quotes.id,
+      quoteText: quotes.quoteText,
+      profanity: quotes.profanity
+    })));
 };
+
